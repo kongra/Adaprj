e809b4a923f66d6b43e3f608216b97248bf302d0
Konrad Grzanek <kongra@gmail.com>
2019-02-10 22:42:52 +0100

    Improved flags.

--- make-release.sh
+++ make-release.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-gprbuild -d -P/home/kongra/Pulpit/Adaprj/adaprj.gpr -j2 --subdirs=opt -cargs:Ada -O3 -march=native -Wall -Wuninitialized -pedantic -gnatf -gnatVa -gnatU -gnatwcdfijklmopruvz -gnatn -cargs:C -O3 -march=native -Wall -pedantic -cargs:C++ -O3 -march=native -Wall -pedantic
+# Consider adding -gnatVa (selected validity checking mode)
+gprbuild -d -P/home/kongra/Pulpit/Adaprj/adaprj.gpr -j2 --subdirs=opt -cargs:Ada -O3 -march=native -Wall -Wuninitialized -pedantic -gnatf -gnatU -gnatwcdfijklmopruvz -gnatn -cargs:C -O3 -march=native -Wall -pedantic -cargs:C++ -O3 -march=native -Wall -pedantic
--- src/main.adb
+++ src/main.adb
@@ -1,8 +1,45 @@
+--  -*- Mode: Ada -*-
 with Ada.Text_IO;
+use  Ada.Text_IO;
 
 procedure Main is
-   type Sample is range 1 .. 10;
-   S : Sample := -2;
+   function Fib(N : in Natural) return Natural is
+   begin
+      if N = 0 or N = 1 then
+         return N;
+      else
+         return Fib(N - 1) + Fib(N - 2);
+      end if;
+   end Fib;
+
+   type Fibcount is
+      record
+         Value : Natural;
+         Count : Natural;
+      end record;
+
+   function Count(N : in Natural) return Fibcount is
+   begin
+      if N = 0 or N = 1 then
+         return (Value => N, Count => 1);
+      else
+         declare
+            C1 : constant Fibcount := Count(N - 1);
+            C2 : constant Fibcount := Count(N - 2);
+         begin
+            return (Value => C1.Value + C2.Value,
+                    Count => C1.Count + C2.Count + 1);
+         end;
+      end if;
+   end Count;
+
 begin
-   Ada.Text_IO.Put_Line("Aab");
+   -- Put_Line(Natural'Last'Image);
+   -- Put_Line(Fib(43)'Image);
+   -- Put_Line(Fib(44)'Image);
+   -- Put_Line(Fib(45)'Image);
+
+   Put_Line(Count(41).Count'Image);
+   Put_Line(Count(42).Count'Image);
+   Put_Line(Count(43).Count'Image);
 end Main;
