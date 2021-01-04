c3383a61042eab0bdf0a62e0bfc7cca5082270da
kongra <kongra@gmail.com>
2021-01-04 19:41:04 +0100

    Cleanup.

--- src/main.adb
+++ src/main.adb
@@ -1,19 +1,11 @@
 procedure Main is
 
-   Incremented : Boolean := False;
-
-   procedure Incr_Until (X : in out Natural) with
-     Contract_Cases =>
-     (Incremented => X > X'Old,
-      others      => X = X'Old);
+   procedure Incr_Until (X : in out Natural);
 
    procedure Incr_Until (X : in out Natural) is
    begin
       if X < 1000 then
          X := X + 1;
-         Incremented := True;
-      else
-         Incremented := False;
       end if;
    end Incr_Until;
 
