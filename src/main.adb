b498327bbc3082f191d45be76cb7a9ca230e9219
kongra <kongra@gmail.com>
2021-02-06 21:09:41 +0100

    Cleanup.

--- src/main.adb
+++ src/main.adb
@@ -1,16 +1,4 @@
 procedure Main is
-
-   procedure Incr_Until (X : in out Natural);
-
-   procedure Incr_Until (X : in out Natural) is
-   begin
-      if X < 1000 then
-         X := X + 1;
-      end if;
-   end Incr_Until;
-
-   X1 : Natural := 10;
-
 begin
-   Incr_Until (X1);
+   null;
 end Main;
